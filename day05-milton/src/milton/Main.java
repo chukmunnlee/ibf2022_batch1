bb711e7a8f3ad215dee6144ef37a7fc6bdba07cf
Chuk <chukmunnlee@gmail.com>
2022-12-09 15:07:09 +0800

    day05 PM word analytics

--- day05-milton/src/milton/Main.java
+++ day05-milton/src/milton/Main.java
@@ -2,6 +2,9 @@ package milton;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
 
 public class Main {
 
@@ -17,17 +20,40 @@ public class Main {
         FileReader fr = new FileReader(fileName);
         BufferedReader br = new BufferedReader(fr);
 
-        Integer i = 1;
-        while (i <= 100) {
+        Integer totalWords = 0;
+        Map<String, Integer> wordMap = new HashMap<>();
+
+        for (Integer i = 1; i <= 100; i++) {
             line = br.readLine();
             if (null == line)
                 break;
-            System.out.printf("%d: %s\n", i, line.toUpperCase());
-            i++;
+            //System.out.printf("%d: %s\n", i, line.toUpperCase());
+            String[] words = line.trim().split(" ");
+            totalWords += words.length;
+            for (String w: words) {
+                Integer v = wordMap.getOrDefault(w, 0);
+                v++;
+                wordMap.put(w, v);
+                // if (wordMap.containsKey(w)) {
+                //     Integer v = wordMap.get(w) + 1;
+                //     wordMap.put(w, v);
+                // } else
+                //     wordMap.put(w, 1);
+            }
         }
 
         br.close();
         fr.close();
+
+        Set<String> uniqueWords = wordMap.keySet();
+
+        // for (String w: uniqueWords)
+        //     System.out.printf("> %s: %d\n", w, wordMap.get(w));
+
+        System.out.printf("The number of words in first 100 lines: %d\n"
+                , totalWords);
+        System.out.printf("Number of unque words: %d\n", uniqueWords.size());
+
     }
 
 }
