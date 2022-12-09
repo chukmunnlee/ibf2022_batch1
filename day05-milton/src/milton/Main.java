ad3a776fe2b94278bc280adbf648127eefaf3481
Chuk <chukmunnlee@gmail.com>
2022-12-09 16:12:57 +0800

    day05 word analytics CSV

--- day05-milton/src/milton/Main.java
+++ day05-milton/src/milton/Main.java
@@ -1,12 +1,16 @@
 package milton;
 
 import java.io.BufferedReader;
+import java.io.FileOutputStream;
 import java.io.FileReader;
+import java.io.OutputStreamWriter;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
 
 public class Main {
+
+    public static final String HEADER = "word,count\n";
 
     public static void main(String[] args) throws Exception {
         String fileName = args[0];
@@ -16,7 +20,6 @@ public class Main {
         // Open the file, read the first 100 lines print out each line
         // Close the file when done
         // If the file is less than 100 lines, close when done
-        String line;
         FileReader fr = new FileReader(fileName);
         BufferedReader br = new BufferedReader(fr);
 
@@ -24,23 +27,24 @@ public class Main {
         Map<String, Integer> wordMap = new HashMap<>();
 
         for (Integer i = 1; i <= 100; i++) {
-            line = br.readLine();
+            String line = br.readLine();
             if (null == line)
                 break;
             //System.out.printf("%d: %s\n", i, line.toUpperCase());
             String[] words = line.trim().split(" ");
             totalWords += words.length;
             for (String w: words) {
-                Integer v = wordMap.getOrDefault(w, 0);
+                String clearWord = w.replaceAll(",", "");
+                Integer v = wordMap.getOrDefault(clearWord, 0);
                 v++;
-                wordMap.put(w, v);
+                wordMap.put(clearWord, v);
                 // if (wordMap.containsKey(w)) {
                 //     Integer v = wordMap.get(w) + 1;
                 //     wordMap.put(w, v);
                 // } else
                 //     wordMap.put(w, 1);
-            }
-        }
+            } // for
+        } // for
 
         br.close();
         fr.close();
@@ -54,6 +58,20 @@ public class Main {
                 , totalWords);
         System.out.printf("Number of unque words: %d\n", uniqueWords.size());
 
-    }
+        // Create CSV file
+        FileOutputStream fos = new FileOutputStream(args[1]);
+        OutputStreamWriter osw = new OutputStreamWriter(fos);
+
+        osw.write(HEADER);
+        for (String w: wordMap.keySet()) {
+            String line = String.format("%s,%d\n", w, wordMap.get(w));
+            osw.write(line);
+        }
+
+        osw.flush();
+        osw.close();
+        fos.close();
+
+    } // main
 
 }
