3e14ba467c278b27ad8693eaa8d3f31b0feaeda6
chukmunnlee <chukmunnlee@gmail.com>
2023-02-15 08:22:45 +0800

    day28 template

---
+++ day28/src/main/java/ibf2022/paf/day28/Day28Application.java
@@ -0,0 +1,16 @@
+package ibf2022.paf.day28;
+
+import java.util.Date;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day28Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Day28Application.class, args);
+	}
+
+}
---
+++ day28/src/test/java/ibf2022/paf/day28/Day28ApplicationTests.java
@@ -0,0 +1,13 @@
+package ibf2022.paf.day28;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Day28ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
