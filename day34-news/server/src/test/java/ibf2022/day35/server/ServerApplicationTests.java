651e805bd61fbcd0c666a1ec8c3b5248971b16aa
chukmunnlee <chukmunnlee@gmail.com>
2023-03-17 08:20:54 +0800

    day35 prep

---
+++ day34-news/server/src/test/java/ibf2022/day35/server/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package ibf2022.day35.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
